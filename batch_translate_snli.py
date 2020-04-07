69c46f4ac23c7a0bb13c1d7dd6619c89a2e7bb37
Robin <rojowiec@hotmail.com>
2020-04-08 00:18:03 +0200

    made translation utils more robust

--- batch_translate_snli.py
+++ batch_translate_snli.py
@@ -14,52 +14,44 @@ from argparse import Namespace
 import pandas as pd
 # load translator
 from onmt.translate.translator import build_translator
-from pandas import DataFrame
 from tqdm import tqdm
 
-from translate_utils import sent_tokenize, detokenize
+from translate_utils import translate_batch
 
+# define global settings
+data_path = "data/"
+# files = list(filter(lambda x: x.endswith(".txt"), os.listdir(data_path)))
+# files = ["snli_1.0_dev.txt"]
+files = ["multinli_1.0_train.txt"]
+batch_size = 128
+beam_size = 16  # the higher the beam size the longer the translation runs
+max_length = 50
 path = 'averaged-10-epoch.pt'
-opt = Namespace(alpha=0.0, batch_type='sents', beam_size=8, beta=-0.0, block_ngram_repeat=0, coverage_penalty='none',
+
+# init translation model
+opt = Namespace(alpha=0.0, batch_type='sents', beam_size=beam_size, beta=-0.0, block_ngram_repeat=0,
+                coverage_penalty='none',
                 data_type='text', dump_beam='', fp32=False, gpu=0, ignore_when_blocking=[], length_penalty='none',
-                max_length=100, max_sent_length=None, min_length=0, models=['pretrained_models/averaged-10-epoch.pt'],
+                max_length=max_length, max_sent_length=None, min_length=0,
+                models=['pretrained_models/averaged-10-epoch.pt'],
                 n_best=1, output="/log", phrase_table='', random_sampling_temp=1.0, random_sampling_topk=1, ratio=-0.0,
                 replace_unk=False, report_align=False, report_time=False, seed=829, stepwise_penalty=False, tgt=None,
                 verbose=False)
 translator = build_translator(opt, report_score=False)
 
-
-# around 14 translations/second
-
-def translate_batch(df: DataFrame, start, idx):
-    label = df[start: idx]["gold_label"].tolist()
-    sent1 = df[start: idx]["sentence1"].map(lambda x: sent_tokenize(x)).tolist()
-    sent2 = df[start: idx]["sentence2"].map(lambda x: sent_tokenize(x)).tolist()
-
-    probs, sent1_trans = translator.translate(sent1, batch_size=len(sent1))
-    probs, sent2_trans = translator.translate(sent2, batch_size=len(sent2))
-
-    sent1_trans_detok = [detokenize(x) for x in sent1_trans]
-    sent2_trans_detok = [detokenize(x) for x in sent2_trans]
-
-    return [sent1_trans_detok, sent2_trans_detok, label]
-
-
-data_path = "data/"
-files = list(filter(lambda x: x.endswith(".txt"), os.listdir(data_path)))
-batch_size = 64
-
+# iterate over files and translate csv content
 for file in files:
+    print("Translating file %s" % file)
     file_path = os.path.join(data_path, file)
-    out_file_path = "output/" + file + ".csv"
+    out_file_path = "output/" + file[:-4] + ".de.csv"
     if os.path.exists(out_file_path):
         os.remove(out_file_path)
 
-    df = pd.read_csv(file_path, sep='\t', header=0, nrows=300000)
+    df = pd.read_csv(file_path, sep='\t', header=0, error_bad_lines=False)
     all_data = [["sentenceA", "sentenceB", "label"]]
     min_step = min(len(df), batch_size)
     for idx in tqdm(range(0, len(df), batch_size)):
-        data = translate_batch(df, idx, idx + min_step)
+        data = translate_batch(translator, df, idx, idx + min_step, max_length)
         start = idx
 
         for i in range(len(data[0])):
--- translate_utils.py
+++ translate_utils.py
@@ -7,21 +7,40 @@ Filename: translate_utils.py
 Date: 05.04.2020
 
 """
+import sys
+
 import sentencepiece as spm
+from pandas import DataFrame
 
 sp = spm.SentencePieceProcessor()
 sp.Load("pretrained_models/sentencepiece.model")
 
 
-def sent_tokenize(text: str):
+def translate_batch(translator, df: DataFrame, start, idx, max_length):
+    label = df[start: idx]["gold_label"].tolist()
+    sent1 = df[start: idx]["sentence1"].map(lambda x: sent_tokenize(x, max_length)).tolist()
+    sent2 = df[start: idx]["sentence2"].map(lambda x: sent_tokenize(x, max_length)).tolist()
+
+    probs, sent1_trans = translator.translate(sent1, batch_size=len(sent1))
+    probs, sent2_trans = translator.translate(sent2, batch_size=len(sent2))
+
+    sent1_trans_detok = [detokenize(x) for x in sent1_trans]
+    sent2_trans_detok = [detokenize(x) for x in sent2_trans]
+
+    return [sent1_trans_detok, sent2_trans_detok, label]
+
+
+def sent_tokenize(text: str, max_length=sys.maxsize):
     """
     Apply sentence tokenization and return tokenized
     sentence as string
     :param text:
     :return:
     """
-    pieces = sp.EncodeAsPieces(text)
-    return " ".join(pieces)
+    if text is not None and type(text) == str and len(text.strip().split()) > 0:
+        pieces = sp.EncodeAsPieces(text)[:max_length]
+        return " ".join(pieces)
+    return str(text)
 
 
 def detokenize(text: list):
